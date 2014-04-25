f9992d93945e4687a9af646e7bb231ef3bafcf12
junohu <thomasfromderson@gmail.com>
2014-04-25 17:10:09 +0200

    Depot initial

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'sqlite3-ruby', '1.3.2', :group => :development
+
+group :development do
+  gem 'rspec-rails', '2.5.0'
+end
+
+group :test do
+  gem 'rspec', '2.5.0'
+  gem 'webrat', '0.7.1'
+end
+
+group :production do
+  gem "pg", "~> 0.11.0"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a9cd520a4a14e451564c6035b39c47131424b64646fc95be2b9f9c744488cfaa1601a736afb3936443898d7200495b4d79bdc980196819215f6edcb6a7df278a'
